7d5124e6b5e5edba8a5e58d83df28225dc5ed7ac
cmagnus <cmagnus@gmail.com>
2017-02-16 14:31:48 -0500

    Adds properties and constructor

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,8 +1,15 @@
 public class TowerOfHanoi{
+  // Properties
+  private int numberOfDiscs;
+  private int[][] towers;
   
-  
+  // Constructors
+  public TowerOfHanoi(int numberOfDiscs) {
+    this.numberOfDiscs = numberOfDiscs;
+    this.towers = new int[numberOfDiscs][3];
+  }
   
   public static void main(String [ ] args) {
-
+    TowerOfHanoi tower = new TowerOfHanoi(4);
   }
 }
