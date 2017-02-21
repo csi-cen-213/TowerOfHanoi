2ee8833fd4662fa77d430dc0da16427080dab0aa
cmagnus <cmagnus@gmail.com>
2017-02-21 14:34:24 -0500

    Adds topDisc method; needs debugging

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,7 +1,7 @@
 public class TowerOfHanoi{
   // Properties
   private int numberOfDiscs;
-  private int[][] towers;
+  public int[][] towers;
   
   // Constructors
   public TowerOfHanoi(int numberOfDiscs) {
@@ -30,10 +30,28 @@ public class TowerOfHanoi{
     }
   }
 
-
+  // Finds index of top of tower
+  public int topDisc(int tower, int index) {
+    if (this.towers[index][0] > 0) { 
+      return(index);
+    }
+    else {
+      topDisc(tower, index+1);
+    }
+    return (index);
+  }
+  
+  // polymorphic version of recursive topDisc method
+  // to not have to pass starting index
+  public int topDisc(int tower) {
+    return this.topDisc(tower, 0);
+  }
   
   public static void main(String [ ] args) {
-    TowerOfHanoi tower = new TowerOfHanoi();
+    TowerOfHanoi tower = new TowerOfHanoi(4);
+    tower.towers[0][0] = 0;
     tower.print();
+    
+    System.out.println(tower.topDisc(0));
   }
 }
